4e3ee8052a0ddef522ec1c44c685334da32c6b92
ols <oli.millington@gmail.com>
2024-05-13 20:59:37 +0700

    only check stories in islands

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -4,7 +4,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
-  stories: ['../../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  stories: ['../../islands/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-essentials'],
   framework: {
     name: '@storybook/web-components-vite',

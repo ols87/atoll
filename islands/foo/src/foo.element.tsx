3b63f9be6295f1d71ef1914bf220a5e980be7f10
ols <oli.millington@gmail.com>
2024-05-13 16:02:31 +0700

    generate islands with tests

--- islands/foo/src/foo.element.tsx
+++
@@ -1,6 +0,0 @@
-import { customElement, noShadowDOM } from 'solid-element';
-
-customElement('atoll-foo', { prop: 'atoll-foo' }, (props) => {
-  noShadowDOM();
-  return <div>{props.prop} works</div>;
-});
--- islands/foo/src/foo.spec.ts
+++
@@ -1,8 +0,0 @@
-import '../../../ui/islands/foo/foo.mjs';
-
-describe('foo', () => {
-  it('work', async () => {
-    document.body.innerHTML = '<atoll-foo prop="foo"></atoll-foo>';
-    expect(document.body.innerHTML).toContain('foo works');
-  });
-});

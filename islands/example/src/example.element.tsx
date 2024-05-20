7c7b6672c7d9848cbe0a10b436a3dfd06f83cf57
ols <oli.millington@gmail.com>
2024-05-20 10:16:27 +0700

    generate and export identity

--- islands/example/src/example.element.tsx
+++ islands/example/src/example.element.tsx
@@ -1,5 +1,10 @@
 import { customElement, noShadowDOM } from 'solid-element';
-import { generateIdentity, exportIdentity, Identity } from '@atoll/sdk';
+import {
+  generateIdentity,
+  exportIdentity,
+  Identity,
+  decryptIdentity,
+} from '@atoll/sdk';
 import { createSignal } from 'solid-js';
 
 customElement('atoll-example', { prop: 'atoll-example' }, (props) => {
@@ -8,15 +13,42 @@ customElement('atoll-example', { prop: 'atoll-example' }, (props) => {
 
   const generateID = async () => setIdentity(await generateIdentity());
 
-  const exportID = async () => {
-    console.log(await exportIdentity());
-  };
+  const exportID = async () => await exportIdentity();
+
+  const decryptID = async () => await decryptIdentity();
 
   return (
     <>
-      {identity()?.publicKey}
+      <style>
+        {`
+        .id {
+          word-break: break-all;
+          padding: 10px;
+          background: #f4f4f4;
+          border-radius: 5px;
+          border: 1px solid #ccc;
+          margin: 10px 0;
+        }
+
+        button {
+          width: 100%;
+          display: block;
+          margin: 10px 0;
+          padding: 10px;
+          background: #333;
+          color: #fff;
+          border-radius: 5px;
+        }
+      `}
+      </style>
+      <div class="id">
+        Public Key:
+        <br />
+        {identity()?.publicKey}
+      </div>
       <button onClick={generateID}>Generate ID</button>
       <button onClick={exportID}>Export Private Key</button>
+      <button onClick={decryptID}>Import ID</button>
     </>
   );
 });
--- sdk/src/lib/identity/identity.ts
+++ sdk/src/lib/identity/identity.ts
@@ -1,9 +1,9 @@
-import { Identities } from '@orbitdb/core';
+import { Identities, KeyStore } from '@orbitdb/core';
 import { createRxDatabase } from 'rxdb';
 import { wrappedKeyEncryptionCryptoJsStorage } from 'rxdb/plugins/encryption-crypto-js';
 import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
 import { generateDynamicSchema } from './identity.schema';
-import { AES } from 'crypto-js';
+import { AES, enc } from 'crypto-js';
 
 export async function generateIdentity(id?: string) {
   id = id || Date.now().toString();
@@ -12,6 +12,8 @@ export async function generateIdentity(id?: string) {
   const identity = await identities.createIdentity({ id });
 
   console.log(identity);
+
+  localStorage.setItem('identity', JSON.stringify(identity));
 
   return identity;
 }
@@ -57,17 +59,17 @@ function rand(length?: number) {
 }
 
 export async function exportIdentity() {
+  const identity = JSON.parse(localStorage.getItem('identity'));
   const keys = await getIdentityKeysFromStore();
   const randomDbName = rand();
   const randomDbPassword = rand();
   const randomCollectionName = rand();
   const randomKeyName = rand();
+  const randomIDName = rand();
 
   const encryptedDexieStorage = wrappedKeyEncryptionCryptoJsStorage({
     storage: getRxStorageDexie(),
   });
-
-  console.log(randomDbName);
 
   const db = await createRxDatabase({
     name: randomDbName,
@@ -81,24 +83,112 @@ export async function exportIdentity() {
       schema: generateDynamicSchema({
         properties: {
           [randomKeyName]: {
-            type: 'array',
+            type: 'string',
+          },
+          [randomIDName]: {
+            type: 'string',
           },
         },
-        encrypted: [randomKeyName],
+        encrypted: [randomKeyName, randomIDName],
       }),
     },
   });
 
-  console.log(collections[randomCollectionName]);
-
   await collections[randomCollectionName].insert({
-    id: rand(),
+    id: randomKeyName,
     [randomKeyName]: AES.encrypt(JSON.stringify(keys), 'foo').toString(),
+    [randomIDName]: AES.encrypt(JSON.stringify(identity), 'foo').toString(),
   });
 
-  return `${randomDbName}:::${randomDbPassword}:::${randomCollectionName}:::${randomKeyName}`;
+  const key = `${randomDbName}:::${randomDbPassword}:::${randomCollectionName}:::${randomKeyName}:::${randomIDName}`;
+
+  localStorage.setItem('key', key);
+
+  console.log(key);
+
+  return key;
 }
 
-// export async function getIdentity(id: string) {
-//   const document = await db.keys.findOne(id).exec();
-// }
+export async function decryptIdentity() {
+  const key = localStorage.getItem('key');
+
+  const [dbName, dbPassword, collectionName, keyName, idName] =
+    key.split(':::');
+
+  const encryptedDexieStorage = wrappedKeyEncryptionCryptoJsStorage({
+    storage: getRxStorageDexie(),
+  });
+
+  const db = await createRxDatabase({
+    name: dbName,
+    storage: encryptedDexieStorage,
+    password: dbPassword,
+    multiInstance: false,
+  });
+
+  const collections = await db.addCollections({
+    [collectionName]: {
+      schema: generateDynamicSchema({
+        properties: {
+          [keyName]: {
+            type: 'string',
+          },
+          [idName]: {
+            type: 'string',
+          },
+        },
+        encrypted: [keyName, idName],
+      }),
+    },
+  });
+
+  const keyEntry = await collections[collectionName].findOne(keyName).exec();
+
+  const decryptedKeys = JSON.parse(
+    AES.decrypt(keyEntry[keyName], 'foo').toString(enc.Utf8),
+  );
+
+  const decryptedID = JSON.parse(
+    AES.decrypt(keyEntry[idName], 'foo').toString(enc.Utf8),
+  );
+
+  console.log(decryptedID);
+
+  localStorage.setItem('identity', JSON.stringify(decryptedID));
+
+  await writeIdentityKeysToStore(new ArrayBuffer(74), decryptedKeys[0]);
+  await writeIdentityKeysToStore(new ArrayBuffer(21), decryptedKeys[1]);
+}
+
+export async function writeIdentityKeysToStore(
+  key: ArrayBuffer,
+  value: { [key: number]: number },
+) {
+  const values = Object.values(value);
+  const uint8 = new Uint8Array(values);
+
+  const openRequest = indexedDB.open('level-js-orbitdb/identities');
+
+  return new Promise((resolve, reject) => {
+    openRequest.onupgradeneeded = function (event) {
+      const db = (event.target as IDBOpenDBRequest).result;
+      db.createObjectStore('orbitdb/identities');
+    };
+
+    openRequest.onsuccess = (database) => {
+      const db = (database.target as IDBOpenDBRequest)?.result;
+
+      if (!db) return reject('Database not found');
+
+      const transaction = db.transaction('orbitdb/identities', 'readwrite');
+      const store = transaction.objectStore('orbitdb/identities');
+
+      const request = store.put(uint8, key);
+
+      request.onsuccess = () => resolve(request.result);
+      request.onerror = (error: Event) => reject(error);
+    };
+
+    openRequest.onerror = (error) => reject(error);
+  });
+}
